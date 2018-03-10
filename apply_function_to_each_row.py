8f06433b0df8befd2bf208de803a8601f10a118a
Lei Shang <lei.shang@hotmail.com>
2018-03-10 18:21:22 +1100

    Minor changes

--- apply_function_to_each_row.py
+++ apply_function_to_each_row.py
@@ -10,13 +10,13 @@ def create_price_label(row):
     return row['name'].title() + " $" + str(row['price'])
 
 
-price = r"""name,price
+fruit_price = r"""name,price
 banana,2.55
 grape,5.35
 apple,4.99
 """
 
-df = pd.read_csv(pd.compat.StringIO(price))
+df = pd.read_csv(pd.compat.StringIO(fruit_price))
 df['label'] = df.apply(create_price_label, axis=1)
 
 print(df)
