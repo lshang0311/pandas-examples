2e8993c3954b1ff73923a77ef70371aa684e2286
Lei Shang <lei.shang@hotmail.com>
2018-03-10 18:19:20 +1100

    Added example

---
+++ apply_function_to_each_row.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+"""
+Apply a function to each row of a DataFrame to create a new column
+
+"""
+
+
+def create_price_label(row):
+    return row['name'].title() + " $" + str(row['price'])
+
+
+price = r"""name,price
+banana,2.55
+grape,5.35
+apple,4.99
+"""
+
+df = pd.read_csv(pd.compat.StringIO(price))
+df['label'] = df.apply(create_price_label, axis=1)
+
+print(df)
