dcdb441785a31949a3df84dfcb2660e1b46e2a7d
Lei Shang <lei.shang@hotmail.com>
2018-03-10 12:45:44 +1100

    Minor changes

--- join_merge_examples.py
+++ join_merge_examples.py
@@ -18,23 +18,21 @@ perth,sa
 df_1 = pd.read_csv(pd.compat.StringIO(weather))
 df_2 = pd.read_csv(pd.compat.StringIO(city))
 
-# inner join
+print("> {join}:".format(join="inner join"))
 df = pd.merge(left=df_1, right=df_2, on='city', how='inner')
 print(df)
+print("\n")
 
-# outer join
+print("> {join}:".format(join="outer join"))
 df = pd.merge(left=df_1, right=df_2, on='city', how='outer')
-print("------------------------------")
 print(df)
+print("\n")
 
-# left outer join
+print("> {join}:".format(join="left outer join"))
 df = pd.merge(left=df_1, right=df_2, on='city', how='left')
-print("------------------------------")
 print(df)
+print("\n")
 
-# right outer join
+print("> {join}:".format(join="right outer join"))
 df = pd.merge(left=df_1, right=df_2, on='city', how='right')
-print("------------------------------")
 print(df)
-
-
