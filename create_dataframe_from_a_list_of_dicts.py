a44e491c788e95bfd840b5a3ffeb9236eb903eb2
Lei Shang <lei.shang@hotmail.com>
2018-03-11 21:13:51 +1100

    Added example

---
+++ create_dataframe_from_a_list_of_dicts.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+list_of_dicts = [
+    {
+        'date': '2018-03-04',
+        'weather': 'cloudy'
+    },
+    {
+        'date': '2018-03-05',
+        'weather': 'sunny'
+    },
+    {
+        'date': '2018-03-06'
+    },
+    {
+        'date': '2018-03-07',
+        'weather': 'rain'
+    }
+]
+
+df = pd.DataFrame(list_of_dicts)
+print(df)
