186d9457a3cb1c76166143592f77c0b6490f961f
Lei Shang <lei.shang@hotmail.com>
2018-03-11 17:20:55 +1100

    Added example

--- use_lambda_to_rename_columns.py
+++ use_lambda_to_rename_columns.py
@@ -7,10 +7,8 @@ Use lambda to change column names
 """
 
 col_names = ['$A', 'B', '$C']
-np.random.seed(0)
-df = pd.DataFrame(np.random.randint(0, 10, (5, 3)),
+df = pd.DataFrame(data=np.random.randint(0, 10, (5, 3)),
                   columns=list(col_names))
-print(df)
 
 df.rename(columns=lambda x: x.replace('$', '') if '$' in x else x, inplace=True)
 print(df)
---
+++ use_list_comprehension_to_rename_columns.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+"""
+Use list comprehension to change column names
+
+"""
+
+col_names = ['$A', 'B', '$C']
+df = pd.DataFrame(
+    data=np.random.randint(0, 10, (5, 3)),
+    columns=list(col_names)
+)
+
+df.columns = [col.strip('$') for col in df.columns]
+print(df)
