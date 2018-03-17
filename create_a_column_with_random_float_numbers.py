ef1d0987af3bb35898f6b3f0fc2f37c1787c5cfe
Lei Shang <lei.shang@hotmail.com>
2018-03-17 22:41:03 +1100

    Added more distributions

--- create_a_column_with_random_float_numbers.py
+++ create_a_column_with_random_float_numbers.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame()
 
-np.random.seed(0)
-df['rand_col_1'] = np.random.uniform(0, 1, size=1000)
-df['rand_col_2'] = np.random.uniform(0, 1, size=1000)
+N = 1000
+df['rand_col_1'] = np.random.RandomState(0).uniform(0, 1, size=N)
+df['rand_col_2'] = np.random.RandomState(0).normal(0, 1, size=N)
+df['rand_col_3'] = np.random.RandomState(0).binomial(10, 0.5, size=N)
+df['rand_col_4'] = np.random.RandomState(1).poisson(3, size=N)
 
-df.hist(bins=5)
+df.hist(bins=10)
 plt.show()
