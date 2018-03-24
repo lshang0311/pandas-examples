d2548331b0673f38f3e1d3a5b5d6e54329bcae9e
Lei Shang <lei.shang@hotmail.com>
2018-03-24 12:43:48 +1100

    Added example - make a time series

---
+++ make_timeseries_dataframe.py
@@ -0,0 +1,20 @@
+import pandas.util.testing as tm
+import pandas as pd
+from dateutil.relativedelta import relativedelta, FR
+import matplotlib.pyplot as plt
+
+# make a df of time series
+tm.N = 10
+df = tm.makeTimeDataFrame(freq='B')  # 'B': business daily
+print(df)
+
+# reindex with new time stamps
+df.index = pd.date_range(
+    start=pd.Timestamp.now().date() + relativedelta(weekday=FR(-1)),
+    periods=df.shape[0],
+    freq='B'
+)
+print(df)
+
+df.plot(figsize=(10, 6), marker='o')
+plt.show()
