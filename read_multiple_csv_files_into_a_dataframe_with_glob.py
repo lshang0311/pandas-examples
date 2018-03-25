ee98216bc633f826cd67e6ae7a2b5bfc37090de1
Lei Shang <lei.shang@hotmail.com>
2018-03-25 06:03:25 -0700

    Minor changes

--- read_multiple_csv_files_into_a_dataframe_with_glob.py
+++ read_multiple_csv_files_into_a_dataframe_with_glob.py
@@ -1,11 +1,13 @@
+import glob
+import os
+
 import pandas as pd
-import glob, os
 
 """
 https://stackoverflow.com/questions/20906474/import-multiple-csv-files-into-pandas-and-concatenate-into-one-dataframe
 
 """
 
-df = pd.concat(map(pd.read_csv, glob.glob(os.path.join('./data', "list_of_fruits_*.csv"))))
+df = pd.concat(map(pd.read_csv, glob.glob(os.path.join('../data', "list_of_fruits_*.csv"))))
 df.reset_index(inplace=True, drop=True)
 print(df)
