4ed073f6d9b6c6ca269346f4743b85da5e0c6740
Lei Shang <lei.shang@hotmail.com>
2018-04-28 11:41:33 +1000

    Added examples

---
+++ use_applymap_for_applying_element_wise_function.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+fruit_price = r"""product,unit_price,total_price
+apple,"$1.56","$1,234,567"
+peach,"$3.33","$2,345,678"
+grape,"$2.22","$3,456,789"
+"""
+
+df = pd.read_csv(pd.compat.StringIO(fruit_price), thousands=r',')
+
+df = df.applymap(lambda x: float((x.replace(',', '').replace('$', ''))) if '$' in x else x)
+print(df)
