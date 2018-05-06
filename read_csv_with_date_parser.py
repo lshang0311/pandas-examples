86cba97c7ba50f988344c212d5849a7462e9ba5d
Lei Shang <lei.shang@hotmail.com>
2018-05-06 18:17:52 +1000

    Added example - read csv with date parser

---
+++ read_csv_with_date_parser.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+data = r"""store,year,month,day,sales
+1,2018,Mon01,D01,100
+3,2018,Mon02,D02,200
+9,2018,Mon03,D20,300
+"""
+
+
+# use function
+def date_parser(date_string):
+    return pd.datetime.strptime(date_string, '%Y Mon%m D%d')
+
+
+df = pd.read_csv(
+    pd.compat.StringIO(data),
+    parse_dates={'DATE': [1, 2, 3]},
+    date_parser=date_parser
+)
+df.set_index(['DATE'], inplace=True)
+assert isinstance(df.index, pd.core.indexes.datetimes.DatetimeIndex)
+print(df)
+
+# use lambda
+df = pd.read_csv(
+    pd.compat.StringIO(data),
+    index_col='date',
+    parse_dates={'date': [1, 2, 3]},
+    date_parser=lambda x: pd.datetime.strptime(x, '%Y Mon%m D%d')
+)
+assert isinstance(df.index, pd.core.indexes.datetimes.DatetimeIndex)
+print(df)
