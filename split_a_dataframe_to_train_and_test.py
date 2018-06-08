03db15c7e538d05dad711405b1193473c2c3028c
Lei Shang <lei.shang@hotmail.com>
2018-06-08 18:06:31 +1000

    Split for validation

--- split_a_dataframe_to_train_and_test.py
+++ split_a_dataframe_to_train_and_test.py
@@ -2,8 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # data
-data = r"""
-date,variable
+data = r"""date,x
 2018-03-01,0
 2018-03-02,1
 2018-03-03,2
@@ -18,12 +17,24 @@ date,variable
 df = pd.read_csv(pd.compat.StringIO(data))
 
 # use pandas sample
-train = df.sample(frac=0.8, random_state=0)
-test = df.drop(train.index)
+train_and_validate = df.sample(frac=0.8, random_state=0)
+test = df.drop(train_and_validate.index)
+
+train = train_and_validate.sample(frac=0.8, random_state=0)
+validate = train_and_validate.drop(train.index)
+
 print(train.sort_index())
+print(validate.sort_index())
 print(test.sort_index())
 
 # use sklearn
 train, test = train_test_split(df, test_size=0.2, random_state=1)
+train, validate = train_test_split(train, test_size=0.2, random_state=1)
+print("train:")
 print(train.sort_index())
+
+print("validate:")
+print(validate.sort_index())
+
+print("test:")
 print(test.sort_index())
